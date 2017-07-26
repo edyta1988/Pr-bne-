7003ce07aa036ccd03bc45dbb0873d63b7967f77
Edyta Zawada <edyta.zawada88@gmail.com>
2017-07-26 20:06:43 +0200

    gruntfile file change

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,9 +3,62 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
 
+  sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'css/main.css': 'sass/main.sass'
+        }
+      }
+    }
+
+    imagemin: {
+    dynamic: {
+        files: [{
+            expand: true,
+            cwd: 'images/',
+            src: ['**/*.{png,jpg,gif}'],
+            dest: 'images/build/'
+        }]
+    }
+}
+
+watch: {
+    scripts: {
+        files: ['sass/*.sass'],
+        tasks: ['sass'],
+        options: {
+            spawn: false,
+        },
+    }
+}
+
+var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+ 
+module.exports = {
+  // ... 
+  plugins: [
+    new BrowserSyncPlugin({
+      // browse to http://localhost:3000/ during development, 
+      // ./public directory is being served 
+      host: 'localhost',
+      port: 3000,
+      server: { baseDir: ['public'] }
+    })
+  ]
+}
+
   });
   // Load the plugins tasks
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-webpack --watch');
 
   // Default task(s).
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', [saas, 'imagemin', 'watch']);
 };
+
+
